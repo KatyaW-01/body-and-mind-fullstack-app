0be4666cdf87c12c286254dbafe3f35f309eecac
Katya Weicht <katya.weicht@gmail.com>
2025-08-14 17:57:37 -0600

    Create outline of workout routes

--- server/app.py
+++ server/app.py
@@ -14,3 +14,24 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+@app.route('/api/workouts', methods=["GET"])
+def get_workouts():
+  pass
+
+@app.route('/api/workouts', methods=["POST"])
+def create_workout():
+  pass
+
+@app.route('/api/workouts/<id>', methods=["GET"])
+def get_one_workout():
+  pass
+
+@app.route('/api/workouts/<id>', methods=["PATCH"])
+def update_workout():
+  pass
+
+@app.route('/api/workouts/<id>', methods=["DELETE"])
+def delete_workout():
+  pass
+
