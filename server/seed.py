1971d0e813be47592219600ffc2555e07efc7efa
Katya Weicht <katya.weicht@gmail.com>
2025-08-13 22:21:19 -0600

    Set up dates for mood instances

--- server/seed.py
+++ server/seed.py
@@ -9,8 +9,9 @@ with app.app_context():
   Workout.query.delete()
   WorkoutExercise.query.delete()
   MoodLog.query.delete()
+  Weather.query.delete()
 
-  #add workouts
+  #create workouts
   w1 = Workout(date=date(2025,7,28), type="Running", duration=20, intensity=3, notes="Easy 2 mile run")
   w2 = Workout(date=date(2025,7,30), type="Hiking", duration=180, intensity=4, notes="Beautiful hike in colorado")
   w3 = Workout(date=date(2025,8,4), type="Strength Training", duration=120, intensity=8 ,notes="Intense leg day")
@@ -27,8 +28,30 @@ with app.app_context():
   we2 = WorkoutExercise(workout=w3, name="deadlifts", sets=3, reps=10, weight=65.0)
   we3 = WorkoutExercise(workout=w3, name="hip thrusts", sets=3, reps=8, weight=115.0)
 
-  db.session.add_all([we1,we2,we3])
+  we4 = WorkoutExercise(workout=w6, name="bench press", sets=4, reps=8, weight=65.0)
+  we5 = WorkoutExercise(workout=w6, name="assisted dips", sets=3, reps=10, weight=70.0)
+  we6 = WorkoutExercise(workout=w6, name="incline bench press", sets=3, reps=10, weight=17.5)
+  
+
+  db.session.add_all([we1,we2,we3,we4,we5,we6])
   db.session.commit()
 
-  #add Moods
+  #create moods
+  #user tracks mood every day regardless of if they worked out or not
+  m1 = MoodLog(date=date(2025,7,28), rating=, mood="", notes="") #worked out 
+  m2 = MoodLog(date=date(2025,7,29), rating=, mood="", notes="")
+  m3 = MoodLog(date=date(2025,7,30), rating=, mood="", notes="") #worked out 
+  m4 = MoodLog(date=date(2025,7,31), rating=, mood="", notes="")
+  m5 = MoodLog(date=date(2025,8,1), rating=, mood="", notes="")
+  m6 = MoodLog(date=date(2025,8,2), rating=, mood="", notes="")
+  m7 = MoodLog(date=date(2025,8,3), rating=, mood="", notes="")
+  m8 = MoodLog(date=date(2025,8,4), rating=, mood="", notes="") #worked out 
+  m9 = MoodLog(date=date(2025,8,5), rating=, mood="", notes="") #worked out 
+  m10 = MoodLog(date=date(2025,8,6), rating=, mood="", notes="")
+  m11 = MoodLog(date=date(2025,8,7), rating=, mood="", notes="") #worked out 
+  m12 = MoodLog(date=date(2025,8,8), rating=, mood="", notes="")
+  m13 = MoodLog(date=date(2025,8,9), rating=, mood="", notes="")
+  m14 = MoodLog(date=date(2025,8,10), rating=, mood="", notes="") #worked out
+  m15 = MoodLog(date=date(2025,8,11), rating=, mood="", notes="") #worked out 
+  m16 = MoodLog(date=date(2025,8,12), rating=, mood="", notes="")
  
