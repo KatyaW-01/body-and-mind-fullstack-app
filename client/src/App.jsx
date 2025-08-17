c5d174b5e6b8427934eeb470d54d1ab1ec4e27a6
Katya Weicht <katya.weicht@gmail.com>
2025-08-17 15:24:12 -0600

    Create routes in app component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,28 @@
 import React from "react"
 import {Routes, Route} from "react-router-dom"
 import './App.css'
+import Home from "./pages/Home"
+import Workouts from "./pages/Workouts"
+import MoodTracker from "./pages/MoodTracker"
+import Analytics from "./pages/Analytics"
+import WorkoutForm from "./components/WorkoutForm"
+import MoodForm from "./components/MoodForm"
+import WorkoutList from "./components/WorkoutList"
+import MoodList from "./components/MoodList"
 
 function App() {
   return (
     <Routes>
-      
+      <Route path="/" element={ <Home /> } />
+      <Route path="/workouts" element={ <Workouts /> } >
+        <Route index element={ <WorkoutList /> }/>
+        <Route path="workoutForm" element={< WorkoutForm />} />
+      </Route>
+      <Route path="/moods" element={ <MoodTracker /> } >
+        <Route index element={ <MoodList /> }/>
+        <Route path="moodForm" element={<MoodForm />} />
+      </Route>
+      <Route path="/analytics" element={ <Analytics />} />
     </Routes>
   )
 }
