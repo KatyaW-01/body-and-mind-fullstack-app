5ac5a9749396425dcc29cd0e9ca43f53ec5deddd
Katya Weicht <katya.weicht@gmail.com>
2025-08-17 15:28:38 -0600

    Set up navbar component

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,14 @@
 import React from "react"
+import {Link} from "react-router-dom"
 
 function NavBar() {
   return (
-    <div>
-
-    </div>
+    <nav className="navbar">
+      <Link to="/">Home</Link>
+      <Link to="/workouts">Workout Tracker</Link>
+      <Link to="/moods">Mood Tracker</Link>
+      <Link to="/analytics">Analytics</Link>
+    </nav>
   )
 }
 
