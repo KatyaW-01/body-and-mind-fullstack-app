773be20ebeca0dc9adb4fd13b932456b86061807
Katya Weicht <katya.weicht@gmail.com>
2025-08-18 13:38:47 -0600

    Make api call for all workouts

---
+++ client/src/api/workouts.js
@@ -0,0 +1,10 @@
+export async function fetchWorkouts() {
+  try {
+    const response = await fetch("http://127.0.0.1:5555/api/workouts")
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error("Error fetching workout data:", error)
+    return []
+  }
+}
