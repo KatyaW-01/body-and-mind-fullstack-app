77583f0ea87851825082fbb50f587c2feb4a7e07
Katya Weicht <katya.weicht@gmail.com>
2025-08-18 14:06:10 -0600

    Iterate through workouts and create a workout card for each one

---
+++ client/src/components/WorkoutCard.jsx
@@ -0,0 +1,9 @@
+function WorkoutCard({workout}) {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default WorkoutCard
--- client/src/components/WorkoutList.jsx
+++ client/src/components/WorkoutList.jsx
@@ -1,11 +1,21 @@
 import React from "react"
 import {useOutletContext} from "react-router-dom"
+import WorkoutCard from "./WorkoutCard"
 
 function WorkoutList() {
   const {workouts} = useOutletContext()
+  if(workouts.length == 0) {
+    return "No workouts tracked. Please add a workout"
+  }
   return (
     <div>
-
+      {
+        workouts.map((workout) => (
+          <div key={workout.id}>
+            <WorkoutCard workout={workout} />
+          </div>
+        ))
+      }
     </div>
   )
 }
