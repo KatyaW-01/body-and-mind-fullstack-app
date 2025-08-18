67ff996b86aa6f3223895fce9a0fe416ec51b33c
Katya Weicht <katya.weicht@gmail.com>
2025-08-18 17:47:36 -0600

    Edit a workout and display on workouts page

--- client/src/components/WorkoutForm.jsx
+++ client/src/components/WorkoutForm.jsx
@@ -1,22 +1,48 @@
 import React from "react"
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import {useState} from "react"
+import { updateWorkout } from "../api/workouts"
 
 function WorkoutForm() {
   const location = useLocation()
   const workout = location.state.workout
+  const [editedWorkout,setEditedWorkout] = useState({date: workout.date, type: workout.type, duration: workout.duration, intensity: workout.intensity, notes: workout.notes})
+
+  const navigate = useNavigate()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const result = updateWorkout(workout.id, editedWorkout)
+    if (!result.error) {
+      alert("Workout successfully updated")
+      navigate("/workouts", { state: { editedWorkout } });
+    }
+  }
+
+  
+
+  function handleChange(event) {
+    const {name, value} = event.target
+
+    setEditedWorkout(prevWorkout => ({
+      ...prevWorkout, [name]:value
+    }));
+  }
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="date">Date:</label>
-        <input type="text" id="date" name="date" defaultValue={workout.date}/>
+        <input type="text" id="date" name="date" value={editedWorkout.date} onChange={handleChange}/>
         <label htmlFor="type">Type:</label>
-        <input type="text" id="type" name="type" defaultValue={workout.type}/>
+        <input type="text" id="type" name="type" value={editedWorkout.type} onChange={handleChange}/>
         <label htmlFor="duration">Duration:</label>
-        <input type="text" id="duration" name="duration" defaultValue={workout.duration}/>
+        <input type="text" id="duration" name="duration" value={editedWorkout.duration} onChange={handleChange}/>
         <label htmlFor="intensity">Intensity:</label>
-        <input type="text" id="intensity" name="intensity" defaultValue={workout.intensity}/>
+        <input type="text" id="intensity" name="intensity" value={editedWorkout.intensity} onChange={handleChange}/>
         <label htmlFor="notes">Notes:</label>
-        <input type="text" id="notes" name="notes" defaultValue={workout.notes}/>
+        <input type="text" id="notes" name="notes" value={editedWorkout.notes} onChange={handleChange}/>
+        <button type="submit" value="Submit">Submit</button>
       </form>
     </div>
   )
--- client/src/components/WorkoutList.jsx
+++ client/src/components/WorkoutList.jsx
@@ -1,12 +1,29 @@
 import React from "react"
-import {useOutletContext} from "react-router-dom"
+import {useOutletContext, useLocation} from "react-router-dom"
 import WorkoutCard from "./WorkoutCard"
+import {useEffect} from "react"
 
 function WorkoutList() {
   const {workouts, setWorkouts} = useOutletContext()
+  const location = useLocation()
+  const editedWorkout = location.state?.editedWorkout
+
+  useEffect(() => {
+    if(editedWorkout) {
+      setWorkouts(prev => prev.map(w => {
+        if(w.id === editedWorkout.id) {
+          return editedWorkout
+        } else {
+          return w
+        }
+      }))
+    }
+  },[editedWorkout])
+
   if(workouts.length == 0) {
     return "No workouts tracked. Please add a workout"
   }
+
   return (
     <div>
       {
