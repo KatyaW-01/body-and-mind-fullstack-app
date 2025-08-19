de44c3fbbbf4a5922c693befc0dfd2517128610d
Katya Weicht <katya.weicht@gmail.com>
2025-08-19 16:04:49 -0600

    make create workout function with POST api call

--- client/src/api/workouts.js
+++ client/src/api/workouts.js
@@ -36,3 +36,19 @@ export async function updateWorkout(id,content) {
     console.error("Error updating workout:", error)
   }
 }
+
+export async function createWorkout(workout) {
+  try {
+    const response = await fetch("http://127.0.0.1:5555/api/workouts", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(workout)
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error("Error creating workout:", error)
+  }
+}
