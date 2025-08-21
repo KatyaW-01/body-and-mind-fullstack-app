5f5d341093844b6991fe914469c38ad5a5522375
Katya Weicht <katya.weicht@gmail.com>
2025-08-21 13:35:06 -0600

    Pass mood to mood form

--- client/src/components/MoodForm.jsx
+++ client/src/components/MoodForm.jsx
@@ -1,6 +1,14 @@
 import React from "react"
+import {useLocation} from "react-router-dom"
 
 function MoodForm() {
+  const location = useLocation()
+  const mood = location.state.mood
+
+  if (mood.notes === null) {
+    mood['notes'] = ""
+  }
+
   return (
     <div>
 
