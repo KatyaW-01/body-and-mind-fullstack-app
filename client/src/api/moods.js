1519cac18e7d2a038f433b3768a2e13791731edf
Katya Weicht <katya.weicht@gmail.com>
2025-08-21 16:26:27 -0600

    Create function for patch request

--- client/src/api/moods.js
+++ client/src/api/moods.js
@@ -8,3 +8,19 @@ export async function fetchMoods() {
     return []
   }
 }
+
+export async function updateMood(id, content) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5555/api/moods/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(content)
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error("Error updating mood:", error)
+  }
+}
