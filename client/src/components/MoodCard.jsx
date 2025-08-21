4716404e011d87161f9ebb9784700fdd80236ef6
Katya Weicht <katya.weicht@gmail.com>
2025-08-21 13:27:52 -0600

    Render all moods and create buttons for edit, delete, and add new mood

--- client/src/components/MoodCard.jsx
+++ client/src/components/MoodCard.jsx
@@ -1,9 +1,16 @@
 import React from "react"
 
-function MoodCard() {
+function MoodCard({mood, setMoods}) {
   return (
     <div>
-
+      <h3>{mood.date}</h3>
+      <p>Mood: {mood.mood}</p>
+      <p>Rating: {mood.rating}/10</p>
+      {mood.notes ? <p>Notes: {mood.notes}</p> : ""}
+      <div>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
   )
 }
