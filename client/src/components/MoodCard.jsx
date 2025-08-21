d1c5657ae71f0b8d0ff2d930425965550073bd90
Katya Weicht <katya.weicht@gmail.com>
2025-08-21 13:16:45 -0600

    Render a MoodCard for each mood in the data

---
+++ client/src/components/MoodCard.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function MoodCard() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default MoodCard
--- client/src/components/MoodList.jsx
+++ client/src/components/MoodList.jsx
@@ -1,9 +1,23 @@
 import React from "react"
+import {useOutlet, useOutletContext} from "react-router-dom"
+import MoodCard from "./MoodCard"
 
 function MoodList() {
+  const {moods, setMoods} = useOutletContext()
+
+  if(moods.length === 0) {
+    return <p>No moods tracked. Please add a mood.</p>
+  }
+
   return (
     <div>
-
+      {
+        moods.map((mood) => (
+          <div key={mood.id}> 
+            <MoodCard mood={mood} setMoods={setMoods} />
+          </div>
+        ))
+      }
     </div>
   )
 }
