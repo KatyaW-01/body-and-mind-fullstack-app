b30b369edbae6cdc96fdfeac7f4f03357c221010
Katya Weicht <katya.weicht@gmail.com>
2025-08-21 16:52:27 -0600

    Make patch request upon submit and add error handling to edit form

--- client/src/components/MoodForm.jsx
+++ client/src/components/MoodForm.jsx
@@ -1,30 +1,78 @@
 import React from "react"
 import {useState} from "react"
-import {useLocation} from "react-router-dom"
+import {useLocation, useOutletContext, useNavigate} from "react-router-dom"
+import { updateMood, fetchMoods } from "../api/moods"
 
 function MoodForm() {
   const location = useLocation()
   const mood = location.state.mood
-
+  
   const [editedMood, setEditedMood] = useState({id: mood.id, date: mood.date, mood: mood.mood, rating: mood.rating, notes: mood.notes})
+
+  const [errors, setErrors] = useState({})
+
+  const {setMoods} = useOutletContext()
+
+  const navigate = useNavigate()
 
   if (mood.notes === null) {
     mood['notes'] = ""
   }
 
+  console.log(editedMood)
   const today = new Date().toISOString().split("T")[0]
+  
+  async function handleSubmit(event) {
+    event.preventDefault()
+    //if notes is empty make it null
+    const newMood = {
+      ...editedMood,
+      notes: editedMood.notes === "" ? null : editedMood.notes
+    }
+
+    //remove id before making the patch request
+    const {id, ...updatedMood} = newMood
+
+    const result = await updateMood(id, updatedMood)
+
+    if(result.error) {
+      setErrors(result.error)
+      alert("Error updating mood, please try again!")
+      return
+    }
+
+    if(!result.error) {
+      alert("Mood successfully updated")
+
+      const updatedMoodData = await fetchMoods()
+      setMoods(updatedMoodData)
+    }
+  }
+
+  function handleChange(event) {
+    const {name, value} = event.target
+
+    setEditedMood(prevMood => ({
+      ...prevMood, [name]:value
+    }))
+  }
+
+  function handleFinish() {
+    navigate("/moods");
+  }
 
   return (
     <div>
       <h3>Edit your Mood</h3>
-      <form>
+      <form onSubmit={handleSubmit}> 
         <div>
           <label htmlFor="date">Date:</label>
-          <input type="date" id="date" name="date" max={today} value={editedMood.date}/>
+          <input type="date" id="date" name="date" max={today} value={editedMood.date} onChange={handleChange} />
+          {errors.date && <p className="error">{errors.date[0]}</p>}
         </div>
         <div>
           <label htmlFor="mood">Mood:</label>
-          <select id="mood" name="mood" value={editedMood.mood} >
+          <select id="mood" name="mood" value={editedMood.mood} onChange={handleChange} >
             <option value="happy">Happy</option>
             <option value="sad">Sad</option>
             <option value="angry">Angry</option>
@@ -33,20 +81,26 @@ function MoodForm() {
             <option value="excited">Excited</option>
             <option value="tired">Tired</option>
           </select>
+          {errors.mood && <p className="error">{errors.mood[0]}</p>}
         </div>
         <div>
           <label htmlFor="rating">Rating:</label>
-          <input type="range"  min="1" max="10" id="rating" name="rating" value={editedMood.rating}/>
+          <input type="range"  min="1" max="10" id="rating" name="rating" value={editedMood.rating} onChange={handleChange}/>
           <p>value: {editedMood.rating}</p>
+          {errors.rating && <p className="error">{errors.rating[0]}</p>}
         </div>
         <div>
           <label htmlFor="notes">Notes:</label>
-          <input type="text" id="notes" name="notes" value={editedMood.notes} />
+          <input type="text" id="notes" name="notes" value={editedMood.notes} onChange={handleChange} />
+          {errors.notes && <p className="error">{errors.notes[0]}</p>}
         </div>
         <div>
           <button type="submit" value="Submit">Submit</button>
         </div>
       </form>
+      <div>
+        <button onClick={handleFinish}>Finish</button>
+      </div>
     </div>
   )
 }
