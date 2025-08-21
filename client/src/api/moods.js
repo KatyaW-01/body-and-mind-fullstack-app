aae3bd9337284bd2084e43a3c797900abf1f4be0
Katya Weicht <katya.weicht@gmail.com>
2025-08-21 17:18:26 -0600

    Make function for adding a mood with POST request

--- client/src/api/moods.js
+++ client/src/api/moods.js
@@ -36,3 +36,19 @@ export async function deleteMood(id) {
     console.error("Error deleting mood:", error)
   }
 }
+
+export async function createMood(mood) {
+  try {
+    const response = await fetch("http://127.0.0.1:5555/api/moods", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(mood)
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error("Error creating mood:", error)
+  }
+}
