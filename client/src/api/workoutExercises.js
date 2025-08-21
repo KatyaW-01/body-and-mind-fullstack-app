aa3a9bef3a60f1085612f45682cc0c79adc86e98
Katya Weicht <katya.weicht@gmail.com>
2025-08-20 19:55:32 -0600

    Create function for deleting an exercise

--- client/src/api/workoutExercises.js
+++ client/src/api/workoutExercises.js
@@ -13,3 +13,15 @@ export async function updateWorkoutExercise(workout_id, id, content) {
     console.error("Error updating workout exercise:", error)
   }
 }
+
+export async function deleteWorkoutExercise(workout_id, id) {
+  try {
+    const response = await fetch(`http://127.0.0.1:5555/api/workouts/${workout_id}/exercises/${id}`, {
+      method: "DELETE"
+    })
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error("Error deleting exercise:", error)
+  }
+}
