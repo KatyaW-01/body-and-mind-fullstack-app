1d7d5910ec8cfe4f268057fd416c5ee4887ab745
Katya Weicht <katya.weicht@gmail.com>
2025-08-22 11:54:12 -0600

    Create data array needed for the graph

--- client/src/pages/Analytics.jsx
+++ client/src/pages/Analytics.jsx
@@ -1,21 +1,46 @@
 import React from "react"
 import NavBar from "../components/NavBar"
-import {
-  ComposedChart,
-  Line,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend
-} from "recharts"
+import {useEffect, useState} from "react"
+import { fetchWorkouts } from "../api/workouts"
+import { fetchMoods } from "../api/moods"
+import sortArray from "sort-array";
+import {ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from "recharts"
 
 function Analytics() {
+  const [workouts, setWorkouts] = useState([])
+  const [moods, setMoods] = useState([])
+    
+  useEffect(() => {
+    fetchWorkouts().then(data => setWorkouts(data))
+  },[])
+
+  useEffect(() => {
+      fetchMoods().then(data => setMoods(data))
+  },[])
+
+  //make sure dates are in chronological order
+  const sortedMoods = sortArray(moods, {by: "date"})
+  const sortedWorkouts = sortArray(workouts, {by: "date"})
+
+  //create object where key is date and value is workout intensity
+  const workoutByDate = {}
+  sortedWorkouts.forEach(workout => {
+    workoutByDate[workout.date] = workout.intensity
+  })
+
+  const data = sortedMoods.map((mood) => {
+    return {
+      date: mood.date, 
+      moodRating: mood.rating, 
+      mood: mood.mood, 
+      //if workoutByDate has a value for the date otherwise null
+      workoutIntensity: workoutByDate[mood.date] ?? null
+    }
+  })
+
   return(
     <div>
       <NavBar />
-      <p>graph with workout and mood data will go here</p>
     </div>
   )
 }
